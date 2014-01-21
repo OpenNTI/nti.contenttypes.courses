e9380d38b391a4d2a9c2b903c52e8c08b70f7fe7
Jason Madden <jason.madden@nextthought.com>
2014-01-21 16:41:30 +0000

    Those nodes shouldn't be persistent
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@31013 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/outlines.py
+++ src/nti/contenttypes/courses/outlines.py
@@ -19,9 +19,18 @@ from .interfaces import ICourseOutlineCalendarNode
 from .interfaces import ICourseOutlineContentNode
 from nti.dataserver.interfaces import ITitledDescribedContent
 
-from zope.container.ordered import OrderedContainer
-from nti.dataserver.containers import _CheckObjectOnSetMixin
-from nti.dataserver.datastructures import PersistentCreatedModDateTrackingObject
+# We have no need or desire for these nodes to be Persistent (yet)
+# so we cannot extend OrderedContainer:
+# from zope.container.ordered import OrderedContainer
+# Instead, we extend a regular OrderedDict and implement the container part ourself
+from collections import OrderedDict
+# Also note that the mixin:
+# from nti.dataserver.containers import _CheckObjectOnSetMixin
+# doesn't work with the OrderedContainer, so we have to do that ourself
+from zope.container.constraints import checkObject
+from zope.container.contained import Contained, setitem, uncontained
+
+from nti.dataserver.datastructures import CreatedModDateTrackingObject
 
 from nti.utils.schema import createDirectFieldProperties
 from nti.utils.schema import createFieldProperties
@@ -29,8 +38,7 @@ from nti.utils.schema import SchemaConfigured
 from nti.utils.schema import AdaptingFieldProperty
 
 @interface.implementer(ICourseOutlineNode)
-class CourseOutlineNode(_CheckObjectOnSetMixin,
-						OrderedContainer):
+class CourseOutlineNode(Contained, OrderedDict):
 	createFieldProperties(ITitledDescribedContent)
 	createDirectFieldProperties(ICourseOutlineNode)
 	__external_can_create__ = False
@@ -41,6 +49,27 @@ class CourseOutlineNode(_CheckObjectOnSetMixin,
 	def append(self, node):
 		name = unicode(len(self))
 		self[name] = node
+
+	def __setitem__(self, key, value):
+		checkObject(self, key, value)
+		setitem(self, super(CourseOutlineNode,self).__setitem__, key, value)
+
+	def __delitem__(self, key):
+		uncontained(self[key], self, key)
+		super(CourseOutlineNode,self).__delitem__(key)
+
+	def updateOrder(self, order):
+		raise TypeError("Ordering cannot be changed")
+
+	def __reduce__(self):
+		raise TypeError("Not meant to be persistent")
+
+	def __hash__(self):
+		# We inherit an __eq__ from dict, but dict does not
+		# like to be hashed (because mutable cache keys can be bad)
+		# But we have an ACL and need to be hashable
+		return hash( tuple(self.items() ) )
+
 
 @interface.implementer(ICourseOutlineCalendarNode)
 class CourseOutlineCalendarNode(SchemaConfigured,
@@ -60,7 +89,7 @@ class CourseOutlineContentNode(CourseOutlineCalendarNode):
 	createDirectFieldProperties(ICourseOutlineContentNode)
 
 @interface.implementer(ICourseOutline)
-class CourseOutline(PersistentCreatedModDateTrackingObject,
+class CourseOutline(CreatedModDateTrackingObject,
 					CourseOutlineNode):
 	createDirectFieldProperties(ICourseOutline)
 	__external_can_create__ = False
