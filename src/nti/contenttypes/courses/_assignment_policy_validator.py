dbaa85b5880a5de040afad85b40911e21deea9b9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-12-12 04:12:41 +0000

    check point WIP. policy validator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@55565 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/_assignment_policy_validator.py
+++ src/nti/contenttypes/courses/_assignment_policy_validator.py
@@ -9,10 +9,64 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from nti.assessment.interfaces import IQAssignmentPolicies
+from zope import component
+from zope import interface
 
-def validate_assigment_policies(course):
-	policies = IQAssignmentPolicies(course, None)
-	if not policies:
+from nti.assessment.interfaces import IQAssignment
+from nti.assessment.interfaces import IQAssignmentPolicies
+from nti.assessment.interfaces import IQAssignmentPolicyValidator
+
+from .interfaces import ICourseCatalogEntry
+
+@interface.implementer(IQAssignmentPolicyValidator)
+class AutoGradeAssignmentPolicyValidator(object):
+	
+	def validate(self, ntiid, policy):
+		assignment = component.queryUtility(IQAssignment, name=ntiid)
+		if assignment is None:
+			logger.warn("Could not find assignment with ntiid %s", ntiid)
+			
+		auto_grade = policy.get('auto_grade')
+		if not auto_grade:
+			return
+		total_points = auto_grade.get('total_points')
+		try:
+			assert int(total_points)>0
+		except (TypeError, ValueError):
+			msg = "Invalid total points in policy for %s" % ntiid
+			raise ValueError(msg)
+
+def validate_assigment_policies(course): 
+	return
+	course_policies = IQAssignmentPolicies(course, None)
+	if not course_policies:
 		return
 
+	assignments = course_policies.assignments()
+	policies = {a:course_policies.getPolicyForAssignment(a) for a in assignments}
+	
+	# let's try the course
+	registry = component
+	validator = IQAssignmentPolicyValidator(course, None)
+	if validator is None:
+		# Courses may be ISites
+		try:
+			names = ('',)
+			registry = course.getSiteManager()
+		except LookupError:
+			entry = ICourseCatalogEntry(course, None)
+			if entry:
+				names = (entry.ntiid,)
+		
+		for name in names:
+			try:
+				validator = registry.getUtility(IQAssignmentPolicyValidator, name=name)
+				break
+			except LookupError:
+				validator = None
+	
+	if validator is None:
+		validator = AutoGradeAssignmentPolicyValidator()
+	
+	for k,v in policies.items():
+		validator.validate(k, v)
