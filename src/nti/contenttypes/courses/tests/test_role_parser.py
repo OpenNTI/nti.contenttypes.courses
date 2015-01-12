3f7f7c17ea14e68679f147f35dc08d82977ae153
Josh Zuech <josh.zuech@nextthought.com>
2015-01-12 00:55:08 +0000

    Tests for section instructors in main course public scope
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@57273 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/tests/test_role_parser.py
+++ src/nti/contenttypes/courses/tests/test_role_parser.py
@@ -14,6 +14,7 @@ logger = __import__('logging').getLogger(__name__)
 #disable: accessing protected members, too many methods
 #pylint: disable=W0212,R0904
 
+import os.path
 
 from hamcrest import assert_that
 from hamcrest import is_
@@ -21,12 +22,18 @@ from hamcrest import is_in
 from hamcrest import is_not
 from hamcrest import contains_inanyorder
 
+from zope.interface import directlyProvides
+
+from zope.securitypolicy.interfaces import Allow, Deny
+from zope.security.interfaces import IPrincipal
+
+from nti.contenttypes.courses.interfaces import ES_PUBLIC
+from nti.contenttypes.courses.interfaces import ICourseSubInstance
 
 from .. import courses
 
 from . import CourseLayerTest
 
-import os.path
 from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
 from nti.dataserver.users import User
 
@@ -34,20 +41,20 @@ from .._role_parser import fill_roles_from_key
 
 
 from nti.contentlibrary.filesystem import FilesystemKey
-from zope.securitypolicy.interfaces import Allow, Deny
-from zope.security.interfaces import IPrincipal
 
 class TestRoleParser(CourseLayerTest):
 
-
-	@WithMockDSTrans
-	def test_parse(self):
+	def setup_users(self):
 		unames = ('jmadden', 'steve.johnson@nextthought.com', 'harp4162', 'orig_instructor')
 		users = {}
 		for uname in unames:
 			u = User.create_user(dataserver=self.ds, username=uname)
 			users[uname] = u
+		return users
 
+	@WithMockDSTrans
+	def test_parse(self):
+		users = self.setup_users()
 		path = os.path.join( os.path.dirname(__file__),
 							 'TestSynchronizeWithSubInstances',
 							 'Spring2014',
@@ -82,3 +89,52 @@ class TestRoleParser(CourseLayerTest):
 		for scope in inst.SharingScopes.values():
 			assert_that( users['harp4162'], is_in(scope) )
 			assert_that( users['orig_instructor'], is_not(is_in(scope)))
+
+	@WithMockDSTrans
+	def test_section_parse(self):
+		"Verify section instructor scopes."
+		users = self.setup_users()
+		path = os.path.join( os.path.dirname(__file__),
+							 'TestSynchronizeWithSubInstances',
+							 'Spring2014',
+							 'Gateway',
+							 'Sections',
+							 '03',
+							 'role_info.json')
+		key = FilesystemKey()
+		key.absolute_path = path
+
+		# Setup our course structure
+		parent_course = courses.CourseInstance()
+		section_course = courses.CourseInstance()
+		directlyProvides( section_course, ICourseSubInstance )
+		section_course.__parent__ = courses.CourseSubInstances()
+		section_course.__parent__.__parent__ = parent_course
+
+		self.ds.dataserver_folder._p_jar.add( parent_course )
+		self.ds.dataserver_folder._p_jar.add( section_course )
+
+		parent_course.SharingScopes.initScopes()
+		section_course.SharingScopes.initScopes()
+
+		# Section roles loaded
+		fill_roles_from_key( section_course, key )
+
+		section_instructor = 'steve.johnson@nextthought.com'
+
+		assert_that( section_course.instructors,
+					contains_inanyorder(IPrincipal(users[section_instructor]) ) )
+
+		for scope in section_course.SharingScopes.values():
+			assert_that( users[ section_instructor ], is_in(scope) )
+
+		parent_scopes = parent_course.SharingScopes
+		parent_public_scope = parent_scopes[ES_PUBLIC]
+		for scope in parent_course.SharingScopes.values():
+			# Section instructor is *only* in parent public scope.
+			if scope == parent_public_scope:
+				assert_that( users[ section_instructor ],
+							is_in( parent_public_scope ) )
+			else:
+				assert_that( users[ section_instructor ],
+							is_not( is_in( scope ) ) )
