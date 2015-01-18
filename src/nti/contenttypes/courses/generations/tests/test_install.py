6ec8ce4cf8f4a938cb35bf6824f0430c26b9a687
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-18 02:33:42 +0000

    add test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@57846 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/generations/tests/test_install.py
+++ src/nti/contenttypes/courses/generations/tests/test_install.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-
-
-.. $Id$
-"""
 
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
-#disable: accessing protected members, too many methods
-#pylint: disable=W0212,R0904
-
-import unittest
-from hamcrest import assert_that
 from hamcrest import has_key
+from hamcrest import assert_that
 
-from nti.dataserver.tests.mock_dataserver import DataserverLayerTest
-from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
 from nti.dataserver.tests import mock_dataserver
+from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
+from nti.dataserver.tests.mock_dataserver import DataserverLayerTest
 
 class TestFunctionalInstall(DataserverLayerTest):
 
@@ -29,5 +21,4 @@ class TestFunctionalInstall(DataserverLayerTest):
 		conn = mock_dataserver.current_transaction
 		root = conn.root()
 		generations = root['zope.generations']
-
 		assert_that( generations, has_key('nti.dataserver-AAA.contenttypes.courses'))
