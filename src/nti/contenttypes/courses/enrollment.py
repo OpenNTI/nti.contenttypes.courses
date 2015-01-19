1deb0510cc26ac52c3a2f6e36e9b9d42d103e6ca
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-19 23:34:20 +0000

    make sure we notify ObjectRemovedEvent
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@57933 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/enrollment.py
+++ src/nti/contenttypes/courses/enrollment.py
@@ -130,7 +130,10 @@ def remove_from_container(container, key, event=False):
 	if event:
 		del container[key]
 	else:
+		value = container[key]
 		container._delitemf(key)
+		lifecycleevent.removed(value, container, key)
+		locate(value, parent=None, name=None)
 		try:
 			container.updateLastMod()
 		except AttributeError:
