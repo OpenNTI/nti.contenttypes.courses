ddde6978f421077f40b9de2cff4296632f31a395
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-06 15:48:25 +0000

    remove unused code; test more
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@64646 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/discussions/tests/test_utils.py
+++ src/nti/contenttypes/courses/discussions/tests/test_utils.py
@@ -7,56 +7,32 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
 from hamcrest import assert_that
-from hamcrest import has_entries
 
 from nti.contenttypes.courses.discussions.model import CourseDiscussion
-from nti.contenttypes.courses.discussions.utils import get_topics_ntiids
+from nti.contenttypes.courses.discussions.utils import get_discussion_key
+from nti.contenttypes.courses.discussions.utils import is_nti_course_bundle
+from nti.contenttypes.courses.discussions.utils import get_discussion_scopes
+from nti.contenttypes.courses.discussions.utils import get_discussion_provider
 
 from nti.contenttypes.courses.tests import CourseLayerTest
 
 class TestUtils(CourseLayerTest):
 
-	def test_get_topics_ntiids_ids(self):
-		discussion = CourseDiscussion()
-		discussion.scopes = (u'All',)
-		discussion.id = 'nti-course-bundle://Sections/010/Discussions/d0.json'
-		result = get_topics_ntiids(discussion, provider='LSTD_1153')
-		assert_that(result, 
-					has_entries(
-						'Public', u'tag:nextthought.com,2011-10:LSTD_1153-Topic-Open_Sections_010_Discussions_d0_json',
-						'Purchased', u'tag:nextthought.com,2011-10:LSTD_1153-Topic-Purchased_Sections_010_Discussions_d0_json',
-						'ForCredit', u'tag:nextthought.com,2011-10:LSTD_1153-Topic-ForCredit_Sections_010_Discussions_d0_json',
-						'ForCreditDegree', u'tag:nextthought.com,2011-10:LSTD_1153-Topic-ForCreditDegree_Sections_010_Discussions_d0_json',
-						'ForCreditNonDegree', u'tag:nextthought.com,2011-10:LSTD_1153-Topic-ForCreditNonDegree_Sections_010_Discussions_d0_json'),
-					)
-
-	def test_get_topics_ntiids_title(self):
+	def test_course_bundle(self):
 		discussion = CourseDiscussion()
 		discussion.title = '11:6:Perspectives'
 		discussion.scopes = (u'All',)
-		result = get_topics_ntiids(discussion, provider='LSTD_1153')
-		
-		assert_that(result, 
-					has_entries(
-						'Public', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseRoot-Open_Discussions_11_6_Perspectives',
-						'Purchased', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseRoot-Purchased_Discussions_11_6_Perspectives',
-						'ForCredit', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseRoot-ForCredit_Discussions_11_6_Perspectives',
-						'ForCreditDegree', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseRoot-ForCreditDegree_Discussions_11_6_Perspectives',
-						'ForCreditNonDegree', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseRoot-ForCreditNonDegree_Discussions_11_6_Perspectives')
-					)
+		discussion.id = 'nti-course-bundle://LSTD/Sections/010/Discussions/d0.json'
+		assert_that(is_nti_course_bundle(discussion), is_(True))
+		assert_that(get_discussion_provider(discussion), is_('LSTD'))
+		assert_that(get_discussion_key(discussion), is_('d0.json'))
 
-	def test_get_topics_ntiids_title_section(self):
+	def test_scopes(self):
 		discussion = CourseDiscussion()
 		discussion.title = '11:6:Perspectives'
 		discussion.scopes = (u'All',)
-		result = get_topics_ntiids(discussion, provider='LSTD_1153', is_section=True)
-		
-		assert_that(result, 
-					has_entries(
-						'Public', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseSection-Open_Discussions_11_6_Perspectives',
-						'Purchased', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseSection-Purchased_Discussions_11_6_Perspectives',
-						'ForCredit', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseSection-ForCredit_Discussions_11_6_Perspectives',
-						'ForCreditDegree', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseSection-ForCreditDegree_Discussions_11_6_Perspectives',
-						'ForCreditNonDegree', u'tag:nextthought.com,2011-10:LSTD_1153-Topic:EnrolledCourseSection-ForCreditNonDegree_Discussions_11_6_Perspectives')
-					)
+		discussion.id = 'nti-course-bundle://LSTD/Sections/010/Discussions/d0.json'
+		assert_that(list(get_discussion_scopes(discussion)), 
+					is_([u'ForCredit', u'Public']))
--- src/nti/contenttypes/courses/discussions/utils.py
+++ src/nti/contenttypes/courses/discussions/utils.py
@@ -9,20 +9,13 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import os
 from urllib import unquote
 from urlparse import urlparse
 
 from zope import component
 
-from nti.ntiids.ntiids import make_ntiid
-from nti.ntiids.ntiids import get_provider
-from nti.ntiids.ntiids import make_provider_safe
-from nti.ntiids.ntiids import make_specific_safe
-
-from ..interfaces import ES_ALL
-from ..interfaces import ES_PUBLIC
 from ..interfaces import ENROLLMENT_LINEAGE_MAP
-from ..interfaces import ENROLLMENT_SCOPE_NAMES
 
 from ..interfaces import ICourseCatalog
 
@@ -43,6 +36,13 @@ def get_discussion_provider(discussion):
 		return result
 	return None
 
+def get_discussion_key(discussion):
+	if is_nti_course_bundle(discussion):
+		parts = urlparse(discussion.id)
+		result = os.path.split(unquote(parts.path))
+		return result[1]
+	return None
+
 def get_entry_for_discussion(discussion, catalog=None, registry=component):
 	provider = get_discussion_provider(discussion)
 	catalog = registry.queryUtility(ICourseCatalog) if catalog is None else catalog
@@ -58,32 +58,3 @@ def get_discussion_scopes(discussion):
 		scopes.update(ENROLLMENT_LINEAGE_MAP.get(scope) or ())
 	return scopes
 
-def get_topics_ntiids(discussion, is_section=False, provider=None, base=None):
-	if not provider and not base:
-		raise ValueError( 'Must supply provider' )
-	
-	result = {}
-	is_ncb = is_nti_course_bundle(discussion)
-	if is_ncb:
-		type_postfix = u''
-		ncb_specific = discussion.id[len(NTI_COURSE_BUNDLE) + 3:]
-		ncb_specific = make_specific_safe(ncb_specific)
-	else:
-		ncb_specific = make_specific_safe(discussion.title)
-		type_postfix = ENROLLED_COURSE_SECTION if is_section else ENROLLED_COURSE_ROOT
-		
-	provider = make_provider_safe(provider or get_provider(base))
-	if ES_ALL in discussion.scopes:
-		scopes = ENROLLMENT_SCOPE_NAMES
-	else:
-		scopes = set(discussion.scopes)
-		
-	for scope in scopes:
-		key = scope
-		scope = 'Open' if scope == ES_PUBLIC else scope
-		nttype = 'Topic%s' % type_postfix
-		pre_spec = '%s%s' % (scope, (u'' if is_ncb else '_Discussions') )
-		specific = make_specific_safe("%s_%s" % (pre_spec, ncb_specific))
-		ntiid = make_ntiid(provider=provider, nttype=nttype, specific=specific, base=base)
-		result[key] = ntiid
-	return result
