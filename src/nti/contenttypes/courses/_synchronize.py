d0fad1d01fcb07bba70fd40b9828d506c936d8da
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-23 18:59:15 +0000

    remove unused
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@67652 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/_synchronize.py
+++ src/nti/contenttypes/courses/_synchronize.py
@@ -8,7 +8,6 @@ defines the on-disk layout.
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-from nti.contentlibrary.synchronize import SynchronizationExeception
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
