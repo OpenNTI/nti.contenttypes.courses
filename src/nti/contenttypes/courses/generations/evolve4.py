62e7ca9eeb5581d93d7a6dbc1ac3b75a3ec18699
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-13 04:39:05 +0000

    prepare evolution; not active yet
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@68813 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttypes/courses/generations/evolve4.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+generation 4.
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 4
+
+import zope.intid
+
+from zope import component
+from zope.component.hooks import site as current_site
+
+from ZODB.POSException import POSError
+
+from ..interfaces import ICourseCatalog
+from ..interfaces import ICourseInstance
+from ..interfaces import ICourseEnrollments
+
+from ..index import install_enrollment_catalog
+
+def do_evolve(context, generation=generation):
+
+	conn = context.connection
+	dataserver_folder = conn.root()['nti.dataserver']
+	
+	lsm = dataserver_folder.getSiteManager()
+	intids = lsm.getUtility(zope.intid.IIntIds)
+	
+	catalog = install_enrollment_catalog(dataserver_folder)
+	
+	total = 0
+	sites = dataserver_folder['++etc++hostsites']
+	for site in sites.values():
+		with current_site(site):
+			catalog = component.getUtility(ICourseCatalog)
+			for entry in catalog.iterCatalogEntries():
+				course = ICourseInstance(entry, None)
+				if not course:
+					continue
+				enrollments = ICourseEnrollments(course, None)
+				if not enrollments:
+					continue
+				for e in enrollments.iter_enrollments():
+					uid = intids.queryId(e)
+					if uid is not None:
+						try:
+							catalog.index_doc(uid, e)
+						except (TypeError, POSError):
+							pass
+						
+	logger.info('contenttypes.courses evolution %s done; %s object(s) indexed',
+				generation, total)
+			
+			
+def evolve(context):
+	"""
+	Evolve to generation 4 by indexing all enrollment records
+	"""
+	do_evolve(context, generation)
