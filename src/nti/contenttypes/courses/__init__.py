524f35f62ce9308a8c27efdf3670a22503341dbd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-13 04:25:31 +0000

    helper func
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@68812 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/__init__.py
+++ src/nti/contenttypes/courses/__init__.py
@@ -13,3 +13,12 @@ logger = __import__('logging').getLogger(__name__)
 
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory(__name__)
+
+from zope import component
+
+from zope.catalog.interfaces import ICatalog
+
+from .index import CATALOG_NAME
+
+def get_enrollment_catalog():
+	return component.queryUtility(ICatalog, name=CATALOG_NAME)
