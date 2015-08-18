9f30b5529a70f12b3b5248163a5653f6dd846f10
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-08-18 15:14:45 +0000

    wrap in site
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@70844 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/generations/evolve6.py
+++ src/nti/contenttypes/courses/generations/evolve6.py
@@ -13,7 +13,15 @@ logger = __import__('logging').getLogger(__name__)
 
 generation = 6
 
+from zope import component
+from zope import interface
+
+from zope.component.hooks import site, setHooks
+
 from zope.intid import IIntIds
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
 
 from ..index import IX_USERNAME
 from ..index import UsernameIndex
@@ -21,33 +29,56 @@ from ..index import install_enrollment_catalog
 
 from .evolve4 import do_reindex
 
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+	root = None
+
+	def get_by_oid(self, oid, ignore_creator=False):
+		resolver = component.queryUtility(IOIDResolver)
+		if resolver is None:
+			logger.warn("Using dataserver without a proper ISiteManager configuration.")
+		else:
+			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+		return None
+	
 def do_evolve(context, generation=generation):
 
+	setHooks()
 	conn = context.connection
-	dataserver_folder = conn.root()['nti.dataserver']
+	root = conn.root()
+	dataserver_folder = root['nti.dataserver']
 
-	lsm = dataserver_folder.getSiteManager()
-	intids = lsm.getUtility(IIntIds)
-	catalog = install_enrollment_catalog(dataserver_folder)
+	mock_ds = MockDataserver()
+	mock_ds.root = dataserver_folder
+	component.provideUtility(mock_ds, IDataserver)
 
-	# remove old index
-	index = catalog[IX_USERNAME]
-	index.clear()
-	intids.unregister(index)
-	index.__name__ = None
-	index.__parent__ = None
-	del catalog[IX_USERNAME]
-		
-	# recreate new index
-	index = catalog[IX_USERNAME] = UsernameIndex(family=intids.family)
-	intids.register(index)
-	index.__parent__ = catalog
-	index.__name__ = IX_USERNAME
+	with site(dataserver_folder):
+		assert	component.getSiteManager() == dataserver_folder.getSiteManager(), \
+				"Hooks not installed?"
+
+		lsm = dataserver_folder.getSiteManager()
+		intids = lsm.getUtility(IIntIds)
+		catalog = install_enrollment_catalog(dataserver_folder)
 	
-	sites = dataserver_folder['++etc++hostsites']
-	total = do_reindex(sites, catalog, intids)
-
-	logger.info('contenttypes.courses evolution %s done; %s object(s) indexed',
+		# remove old index
+		index = catalog[IX_USERNAME]
+		index.clear()
+		intids.unregister(index)
+		index.__name__ = None
+		index.__parent__ = None
+		del catalog[IX_USERNAME]
+			
+		# recreate new index
+		index = catalog[IX_USERNAME] = UsernameIndex(family=intids.family)
+		intids.register(index)
+		index.__parent__ = catalog
+		index.__name__ = IX_USERNAME
+		
+		sites = dataserver_folder['++etc++hostsites']
+		total = do_reindex(sites, catalog, intids)
+	
+		logger.info('contenttypes.courses evolution %s done; %s object(s) indexed',
 				generation, total)
 
 def evolve(context):
