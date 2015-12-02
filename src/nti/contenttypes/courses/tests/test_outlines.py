e00a2766c67ca95c00f0b2c2d3f2b9a8099d6dac
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-12-02 04:43:37 +0000

    add test case for field interface tagging
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@78094 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/tests/test_outlines.py
+++ src/nti/contenttypes/courses/tests/test_outlines.py
@@ -7,6 +7,7 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
 from hamcrest import none
 from hamcrest import is_not
 from hamcrest import has_entries
@@ -16,6 +17,8 @@ from nti.testing.matchers import validly_provides
 from nti.testing.matchers import verifiably_provides
 
 from datetime import datetime
+
+from zope.interface.interfaces import IMethod
 
 from nti.coremetadata.interfaces import IRecordable
 
@@ -32,10 +35,28 @@ from nti.contenttypes.courses.tests import CourseLayerTest
 class TestCourseOutline(CourseLayerTest):
 
 	def test_outline_implements(self):
-		assert_that( outlines.CourseOutlineNode(), verifiably_provides(interfaces.ICourseOutlineNode) )
-		assert_that( outlines.CourseOutline(), verifiably_provides(interfaces.ICourseOutline) )
-		assert_that( outlines.CourseOutlineNode(), verifiably_provides(IRecordable))
-		assert_that( outlines.CourseOutline(), verifiably_provides(IRecordable))
+		assert_that(outlines.CourseOutlineNode(), verifiably_provides(interfaces.ICourseOutlineNode))
+		assert_that(outlines.CourseOutline(), verifiably_provides(interfaces.ICourseOutline))
+		assert_that(outlines.CourseOutlineNode(), verifiably_provides(IRecordable))
+		assert_that(outlines.CourseOutline(), verifiably_provides(IRecordable))
+
+	def test_outline_tag_fields(self):
+		fields = ('title', 'description', 'AvailableEnding', 'AvailableBeginning')
+		for iface in (interfaces.ICourseOutlineNode,
+					  interfaces.ICourseOutlineContentNode,
+					  interfaces.ICourseOutlineCalendarNode):
+
+			for name in fields:
+				if name in iface:
+					value = iface[name].queryTaggedValue(interfaces.TAG_HIDDEN_IN_UI)
+					assert_that(value, is_(False))
+					value = iface[name].queryTaggedValue(interfaces.TAG_REQUIRED_IN_UI)
+					assert_that(value, is_(False))
+
+			for k, v in iface.namesAndDescriptions(all=True):
+				if k not in fields and not IMethod.providedBy(v):
+					value = v.queryTaggedValue(interfaces.TAG_HIDDEN_IN_UI)
+					assert_that(value, is_(True))
 
 	def test_trx_history(self):
 		node = outlines.CourseOutlineNode()
@@ -48,12 +69,12 @@ class TestCourseOutline(CourseLayerTest):
 		node = outlines.CourseOutlineNode()
 		outline.append(node)
 		node2 = outlines.CourseOutlineContentNode(ContentNTIID='tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON',
-												  AvailableBeginning=datetime.now() )
+												  AvailableBeginning=datetime.now())
 		node.append(node2)
 
-		assert_that( node, validly_provides(interfaces.ICourseOutlineNode ))
-		assert_that( outline, validly_provides(interfaces.ICourseOutline ))
-		assert_that( inst, validly_provides(interfaces.ICourseInstance ))
+		assert_that(node, validly_provides(interfaces.ICourseOutlineNode))
+		assert_that(outline, validly_provides(interfaces.ICourseOutline))
+		assert_that(inst, validly_provides(interfaces.ICourseInstance))
 
 	def test_outline_externalizes(self):
 
@@ -62,12 +83,13 @@ class TestCourseOutline(CourseLayerTest):
 		node = outlines.CourseOutlineNode()
 		outline.append(node)
 		outline.src = 'src/is/this'
-		node2 = outlines.CourseOutlineContentNode(	ContentNTIID='tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON',
-												 	AvailableBeginning=datetime.now() )
+		node2 = outlines.CourseOutlineContentNode(ContentNTIID='tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON',
+												  AvailableBeginning=datetime.now())
 		outline.append(node2)
 
-		assert_that( inst, externalizes(has_entries('Class', 'CourseInstance',
-													'Outline', has_entries( #'Links', has_item(has_entry('rel', 'Contents')), # A higher level will provide access to contents
-																			'src', 'src/is/this',
-																		   	'MimeType', 'application/vnd.nextthought.courses.courseoutline'),
-													'MimeType', 'application/vnd.nextthought.courses.courseinstance')) )
+		assert_that(inst, externalizes(
+							has_entries('Class', 'CourseInstance',
+										'Outline', has_entries(# 'Links', has_item(has_entry('rel', 'Contents')), # A higher level will provide access to contents
+																'src', 'src/is/this',
+																'MimeType', 'application/vnd.nextthought.courses.courseoutline'),
+										'MimeType', 'application/vnd.nextthought.courses.courseinstance')))
