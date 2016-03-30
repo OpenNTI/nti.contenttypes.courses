560fa2da3c239f928013e7c595b1da465fbc8677
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-30 22:04:43 +0000

    Use known constants
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@85622 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/exporter.py
+++ src/nti/contenttypes/courses/exporter.py
@@ -16,7 +16,9 @@ import simplejson
 from zope import component
 from zope import interface
 
-from nti.contenttypes.courses.interfaces import ICourseExporter 
+from nti.contenttypes.courses.interfaces import SECTIONS
+
+from nti.contenttypes.courses.interfaces import ICourseExporter
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseSubInstance
 from nti.contenttypes.courses.interfaces import ICourseSectionExporter
@@ -31,7 +33,7 @@ class CourseOutlineExporter(object):
 	def export(self, context, filer):
 		course = ICourseInstance(context)
 		if ICourseSubInstance.providedBy(course):
-			bucket = "Sections/%s" % course.__name__
+			bucket = "%s/%s" % (SECTIONS, course.__name__)
 		else:
 			bucket = None
 		# export to json
@@ -40,7 +42,7 @@ class CourseOutlineExporter(object):
 		simplejson.dump(ext_obj, source, indent=4)
 		source.seek(0)
 		# save in filer
-		filer.save("outline.json", source, contentType="text/json", 
+		filer.save("outline.json", source, contentType="text/json",
 				   bucket=bucket, overwrite=True)
 		# save outlines for subinstances
 		for sub_instance in get_course_subinstances(course):
@@ -49,7 +51,7 @@ class CourseOutlineExporter(object):
 
 @interface.implementer(ICourseExporter)
 class CourseExporter(object):
-	
+
 	def export(self, context, filer):
 		course = ICourseInstance(context)
 		for _, exporter in list(component.getUtilitiesFor(ICourseSectionExporter)):
