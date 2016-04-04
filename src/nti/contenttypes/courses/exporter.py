ae1aeab76e023dd40821fe9dc8f76ed850116648
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-04 19:48:43 +0000

    dc_metadata exporter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@85883 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/exporter.py
+++ src/nti/contenttypes/courses/exporter.py
@@ -11,18 +11,23 @@ logger = __import__('logging').getLogger(__name__)
 
 from StringIO import StringIO
 
+from xml.dom import minidom
+
 import simplejson
 
 from zope import component
 from zope import interface
 
-from nti.contenttypes.courses.interfaces import SECTIONS
+from nti.contenttypes.courses.interfaces import SECTIONS, ICourseCatalogEntry
 
 from nti.contenttypes.courses.interfaces import ICourseExporter
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseSubInstance
 from nti.contenttypes.courses.interfaces import ICourseSectionExporter
 
+from nti.contenttypes.courses.common import get_course_packages
+
+from nti.contenttypes.courses.utils import get_parent_course
 from nti.contenttypes.courses.utils import get_course_vendor_info
 from nti.contenttypes.courses.utils import get_course_subinstances
 
@@ -73,6 +78,50 @@ class VendorInfoExporter(object):
 			for sub_instance in get_course_subinstances(course):
 				if sub_instance.Outline is not course.Outline:
 					self.export(sub_instance, filer)
+
+@interface.implementer(ICourseSectionExporter)
+class BundleMetaInfoExporter(object):
+
+	def export(self, context, filer):
+		course = ICourseInstance(context)
+		course = get_parent_course(course)
+		entry = ICourseCatalogEntry(course)
+		data = {u'ntiid':u'',
+				u'title': entry.Title,
+				u"ContentPackages": [x.ntiid for x in get_course_packages(course)]}
+		source = StringIO()
+		ext_obj = to_external_object(data, decorate=False)
+		simplejson.dump(ext_obj, source, indent=4)
+		source.seek(0)
+		# save in filer
+		filer.save("bundle_meta_info.json", source,
+					contentType="application/json", overwrite=True)
+
+@interface.implementer(ICourseSectionExporter)
+class BundleDCMetadataExporter(object):
+
+	def export(self, context, filer):
+		course = ICourseInstance(context)
+		course = get_parent_course(course)
+		entry = ICourseCatalogEntry(course)
+
+		DOMimpl = minidom.getDOMImplementation()
+		xmldoc = DOMimpl.createDocument(None, "metadata", None)
+		doc_root = xmldoc.documentElement
+		doc_root.setAttributeNS(None, "xmlns:dc", "http://purl.org/dc/elements/1.1/")
+
+		for creator in entry.creators or ():
+			node = xmldoc.createElement("dc:creator")
+			node.appendChild(xmldoc.createTextNode(creator))
+			doc_root.appendChild(node)
+
+		node = xmldoc.createElement("dc:title")
+		node.appendChild(xmldoc.createTextNode(entry.Title))
+		doc_root.appendChild(node)
+
+		source = xmldoc.toprettyxml(encoding="UTF-8")
+		for name in ("dc_metadata.xml", "bundle_dc_metadata.xml"):
+			filer.save(name, source, contentType="application/xml", overwrite=True)
 
 @interface.implementer(ICourseExporter)
 class CourseExporter(object):
