9bebe2fa8b8014c09652e6f0ec5f87f3cc5f3528
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-05 23:02:35 +0000

    add vendor info importer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@85987 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/importer.py
+++ src/nti/contenttypes/courses/importer.py
@@ -9,12 +9,50 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import simplejson
+
 from zope import component
 from zope import interface
 
+from nti.contenttypes.courses.interfaces import SECTIONS
+
 from nti.contenttypes.courses.interfaces import ICourseImporter
 from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseSubInstance
 from nti.contenttypes.courses.interfaces import ICourseSectionImporter
+
+from nti.contenttypes.courses.utils import get_course_vendor_info
+from nti.contenttypes.courses.utils import get_course_subinstances
+
+@interface.implementer(ICourseSectionImporter)
+class BaseSectionImporter(object):
+
+	def load(self, source):
+		if hasattr(source, "read"):
+			result = simplejson.load(source)
+		elif hasattr(source, "data"):
+			result = simplejson.loads(source.data)
+		else:
+			result = simplejson.loads(source)
+		return result
+
+@interface.implementer(ICourseSectionImporter)
+class VendorInfoImporter(BaseSectionImporter):
+
+	def process(self, context, filer):
+		course = ICourseInstance(context)
+		if ICourseSubInstance.providedBy(course):
+			bucket = "%s/%s/" % (SECTIONS, course.__name__)
+		else:
+			bucket = u''
+		path = bucket + 'vendor_info.json'
+		source = filer.get(path)
+		if source is not None:
+			verdor_info = get_course_vendor_info(course, True)
+			verdor_info.clear()
+			verdor_info.update(self.load(source))
+		for sub_instance in get_course_subinstances(course):
+			self.process(sub_instance, filer)
 
 @interface.implementer(ICourseImporter)
 class CourseImporter(object):
