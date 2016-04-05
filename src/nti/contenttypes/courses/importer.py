15c4b4fa345f5535a539ab1bb63b057953077a88
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-05 09:53:06 +0000

    add base class
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@85911 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contenttypes/courses/importer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contenttypes.courses.interfaces import ICourseImporter
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseSectionImporter
+
+@interface.implementer(ICourseImporter)
+class CourseImporter(object):
+
+	def process(self, context, filer):
+		course = ICourseInstance(context)
+		for _, importer in list(component.getUtilitiesFor(ICourseSectionImporter)):
+			importer.process(course, filer)
