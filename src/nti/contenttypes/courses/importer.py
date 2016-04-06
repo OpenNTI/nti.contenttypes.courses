7551b4aec58e6bb84d52bc37084ba56e33bad715
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-06 21:23:47 +0000

    outline importer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@86076 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/importer.py
+++ src/nti/contenttypes/courses/importer.py
@@ -25,8 +25,11 @@ from nti.contenttypes.courses.interfaces import ICourseSubInstance
 from nti.contenttypes.courses.interfaces import ICourseSectionImporter
 from nti.contenttypes.courses.interfaces import CourseVendorInfoSynchronized
 
+from nti.contenttypes.courses.utils import clear_course_outline
 from nti.contenttypes.courses.utils import get_course_vendor_info
 from nti.contenttypes.courses.utils import get_course_subinstances
+
+from nti.externalization.internalization import update_from_external_object
 
 @interface.implementer(ICourseSectionImporter)
 class BaseSectionImporter(object):
@@ -34,11 +37,33 @@ class BaseSectionImporter(object):
 	def load(self, source):
 		if hasattr(source, "read"):
 			result = simplejson.load(source)
+		elif hasattr(source, "readContents"):
+			data = source.readContents()
+			result = simplejson.loads(data)
 		elif hasattr(source, "data"):
 			result = simplejson.loads(source.data)
 		else:
 			result = simplejson.loads(source)
 		return result
+
+@interface.implementer(ICourseSectionImporter)
+class CourseOutlineExporter(BaseSectionImporter):
+
+	def process(self, context, filer):
+		course = ICourseInstance(context)
+		if ICourseSubInstance.providedBy(course):
+			bucket = "%s/%s/" % (SECTIONS, course.__name__)
+		else:
+			bucket = u''
+		path = bucket + 'course_outline.json'
+		source = filer.get(path)
+		if source is not None:
+			ext_obj = self.load(source)
+			clear_course_outline(course.Outline) # not merging
+			update_from_external_object(course.Outline, ext_obj, notify=False)
+		for sub_instance in get_course_subinstances(course):
+			if sub_instance.Outline is not course.Outline:
+				self.process(sub_instance, filer)
 
 @interface.implementer(ICourseSectionImporter)
 class VendorInfoImporter(BaseSectionImporter):
@@ -53,7 +78,7 @@ class VendorInfoImporter(BaseSectionImporter):
 		source = filer.get(path)
 		if source is not None:
 			verdor_info = get_course_vendor_info(course, True)
-			verdor_info.clear()
+			verdor_info.clear()  # not merging
 			verdor_info.update(self.load(source))
 			verdor_info.lastModified = time.time()
 			notify(CourseVendorInfoSynchronized(course))
