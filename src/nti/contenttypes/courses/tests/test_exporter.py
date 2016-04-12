9254b4226165f798fa0b5dc337ed53a56f21fc35
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-12 01:06:40 +0000

    test for outline exporter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@86469 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/tests/test_exporter.py
+++ src/nti/contenttypes/courses/tests/test_exporter.py
@@ -9,6 +9,7 @@ __docformat__ = "restructuredtext en"
 
 from hamcrest import is_
 from hamcrest import assert_that
+from hamcrest import greater_than
 
 import os
 import shutil
@@ -16,15 +17,46 @@ import tempfile
 
 from nti.cabinet.filer import DirectoryFiler
 
+from nti.cabinet.mixins import get_file_size
+
+from nti.contentlibrary.filesystem import FilesystemKey
 from nti.contentlibrary.filesystem import FilesystemBucket
+
+from nti.contenttypes.courses._outline_parser import fill_outline_from_key
 
 from nti.contenttypes.courses.courses import ContentCourseInstance
 
+from nti.contenttypes.courses.exporter import CourseOutlineExporter
 from nti.contenttypes.courses.exporter import BundlePresentationAssetsExporter
 
 from nti.contenttypes.courses.tests import CourseLayerTest
 
 class TestExporter(CourseLayerTest):
+
+	def test_export_outline(self):
+		path = os.path.join(os.path.dirname(__file__),
+							'TestSynchronizeWithSubInstances',
+							'Spring2014',
+							'Gateway')
+		inst = ContentCourseInstance()
+		inst.root = FilesystemBucket(name="Gateway")
+		inst.root.absolute_path = path
+		
+		key = FilesystemKey()
+		key.absolute_path = path + '/course_outline.xml'
+
+		outline = inst.Outline
+		fill_outline_from_key(outline, key)
+		tmp_dir = tempfile.mkdtemp(dir="/tmp")
+		try:
+			filer = DirectoryFiler(tmp_dir)
+			exporter = CourseOutlineExporter()
+			exporter.export(inst, filer)
+			path = os.path.join(tmp_dir, 'course_outline.json')
+			assert_that(os.path.exists(path), is_(True))
+			assert_that(get_file_size(path), is_(greater_than(0)))
+		finally:
+			shutil.rmtree(tmp_dir)
 
 	def test_export_presentation_assets(self):
 		path = os.path.join(os.path.dirname(__file__),
