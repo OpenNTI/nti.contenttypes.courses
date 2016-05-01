a5e791e8293ff68ccf50aeb7129eb53a253d25c3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-05-01 01:39:42 +0000

    gather keywords
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@87692 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/adapters.py
+++ src/nti/contenttypes/courses/adapters.py
@@ -9,8 +9,13 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import six
+from collections import Mapping
+
 from zope import component
 from zope import interface
+
+from zope.traversing.api import traverse
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseKeywords
@@ -18,14 +23,36 @@ from nti.contenttypes.courses.interfaces import ICourseKeywords
 from nti.contenttypes.courses.utils import get_course_vendor_info
 
 class _Keywords(object):
-    
-    __slots__ = (b'keywords',)
-    
-    def __init__(self, keywords=()):
-        self.keywords = keywords
+
+	__slots__ = (b'keywords',)
+
+	def __init__(self, keywords=()):
+		self.keywords = keywords
+
+def _keyword_gatherer(data):
+	result = set()
+	if isinstance(data, six.string_types):
+		result.update(data.split())
+	elif isinstance(data, (list, tuple, set)):
+		for value in data:
+			result.update(_keyword_gatherer(value))
+	elif isinstance(data, Mapping):
+		for key, value in data.items():
+			result.add(key)
+			result.update(_keyword_gatherer(value))
+	elif data is not None:
+		result.add(str(data))
+	result = tuple(x.strip().lower() for x in result if x)
+	return result
 
 @component.adapter(ICourseInstance)
 @interface.implementer(ICourseKeywords)
 def _course_keywords(context):
-    get_course_vendor_info(context, create=False)
-    return _Keywords()
+	result = set()
+	data = get_course_vendor_info(context, create=False) or {}
+	data = traverse(data, 'NTI/Keywords', default=None)
+	result.update(_keyword_gatherer(data))
+	data = traverse(data, 'NTI/Tags', default=None)
+	result.update(_keyword_gatherer(data))
+	result.discard(u'')
+	return _Keywords(sorted(result))
