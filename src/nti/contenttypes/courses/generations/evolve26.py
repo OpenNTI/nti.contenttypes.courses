dbdc21588bee4ee99310a14830292a0e487549b8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-30 23:05:41 +0000

    add a course outline node index
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@101713 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/generations/evolve26.py
+++ src/nti/contenttypes/courses/generations/evolve26.py
@@ -9,7 +9,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 25
+generation = 26
 
 from zope import component
 from zope import interface
@@ -18,16 +18,12 @@ from zope.component.hooks import site as current_site
 
 from zope.intid.interfaces import IIntIds
 
-from nti.contenttypes.courses.index import IX_COURSE
-
 from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseOutline
 from nti.contenttypes.courses.interfaces import ICourseInstance
-from nti.contenttypes.courses.interfaces import ICourseEnrollmentManager
-from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
 
-from nti.contenttypes.courses.index import install_enrollment_catalog
+from nti.contenttypes.courses.index import install_course_outline_catalog
 
-from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import IDataserver
 from nti.dataserver.interfaces import IOIDResolver
 
@@ -46,45 +42,22 @@ class MockDataserver(object):
 			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
 		return None
 
-def remove_from_container(container, key, event=False):
-	# Avoid throwing intIdRemovedEvent (analytics tries to store in redis).
-	container._delitemf(key, event=event)
-	try:
-		container.updateLastMod()
-	except AttributeError:
-		pass
-	container._p_changed = True
-
-def unenroll(record, principal_id):
-	try:
-		course = record.CourseInstance
-		enrollment_manager = ICourseEnrollmentManager(course)
-		remove_from_container(enrollment_manager._cat_enrollment_storage, principal_id)
-		remove_from_container(enrollment_manager._inst_enrollment_storage, principal_id)
-	except (TypeError, KeyError):
-		pass
-
-def _clean_up_enrollments(site_name, enroll_catalog, intids, seen, course_catalog):
+def _index_nodes(outline_index, intids, seen, course_catalog):
 	for entry in course_catalog.iterCatalogEntries():
 		if entry.ntiid in seen:
 			continue
 		seen.add(entry.ntiid)
 		course = ICourseInstance(entry, None)
 		if course is not None:
-			# Get enrollment records for course
-			query = { IX_COURSE: {'any_of':(entry.ntiid,)}, }
-			for record_id in enroll_catalog.apply(query) or ():
-				record = intids.queryObject(record_id)
-				if ICourseInstanceEnrollmentRecord.providedBy(record):
-					if IUser(record, None) is None:
-						# If we have no user, remove the record and unenroll;
-						# this is invalid state.
-						# Get our weak_ref principal name.
-						principal = record._principal.username
-						logger.info("[%s] Removing enrollment record for (user=%s) (entry=%s)",
-								 	 site_name, principal, entry.ntiid)
-						unenroll(record, principal)
-						enroll_catalog.unindex_doc(record_id)
+			def recur(node):
+				for child in node.values():
+					recur(child)
+				uid = intids.queryId(node)
+				if uid is not None and not ICourseOutline.providedBy(node):
+					outline_index.index_doc(uid, node)
+
+		if course.Outline:
+			recur(course.Outline)
 
 def do_evolve(context, generation=generation):
 	conn = context.connection
@@ -99,22 +72,20 @@ def do_evolve(context, generation=generation):
 				"Hooks not installed?"
 		lsm = ds_folder.getSiteManager()
 		intids = lsm.getUtility(IIntIds)
-		enroll_catalog = install_enrollment_catalog(ds_folder, intids)
+		outline_index = install_course_outline_catalog(ds_folder, intids)
 
 		seen = set()
 		for site in get_all_host_sites():
 			with current_site(site):
 				course_catalog = component.queryUtility(ICourseCatalog)
 				if course_catalog is not None:
-					site_name = site.__name__
-					_clean_up_enrollments(site_name, enroll_catalog,
-										   intids, seen, course_catalog)
+					_index_nodes(outline_index, intids, seen, course_catalog)
 
 	component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
 	logger.info('Evolution %s done.', generation)
 
 def evolve(context):
 	"""
-	Evolve to generation 25 by removing enrollment records without a valid user.
+	Evolve to generation 256 by installing the outline catalog.
 	"""
 	do_evolve(context, generation)
--- src/nti/contenttypes/courses/index.py
+++ src/nti/contenttypes/courses/index.py
@@ -9,6 +9,8 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import time
+
 from collections import namedtuple
 
 from zope import interface
@@ -30,15 +32,21 @@ from nti.contenttypes.courses.common import get_course_packages
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseKeywords
+from nti.contenttypes.courses.interfaces import ICourseOutlineNode
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+from nti.contenttypes.courses.interfaces import ICourseOutlineCalendarNode
 from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
 
 from nti.zope_catalog.catalog import Catalog
 
+from nti.zope_catalog.datetime import TimestampToNormalized64BitIntNormalizer
+
 from nti.zope_catalog.index import AttributeSetIndex
+from nti.zope_catalog.index import NormalizationWrapper
 from nti.zope_catalog.index import AttributeKeywordIndex
 from nti.zope_catalog.index import SetIndex as RawSetIndex
 from nti.zope_catalog.index import AttributeValueIndex as ValueIndex
+from nti.zope_catalog.index import IntegerValueIndex as RawIntegerValueIndex
 
 # Deprecations
 
@@ -286,6 +294,96 @@ def install_courses_catalog(site_manager_container, intids=None):
 						(IX_PACKAGES, CoursePackagesIndex),
 						(IX_KEYWORDS, CourseKeywordsIndex),
 						(IX_ENTRY, CourseCatalogEntryIndex)):
+		index = clazz(family=intids.family)
+		intids.register(index)
+		locate(index, catalog, name)
+		catalog[name] = index
+	return catalog
+
+# outline catalog
+
+IX_SOURCE = 'source'
+IX_CONTENT_UNIT = 'ContentUnit'
+IX_LESSON_OVERVIEW = 'LessonOverview'
+IX_AVAILABLE_ENDING = 'AvailableEnding'
+IX_AVAILABLE_BEGINNING = 'AvailableBeginning'
+COURSE_OUTLINE_CATALOG_NAME = 'nti.dataserver.++etc++course-outline-catalog'
+
+class NodeSrcIndex(ValueIndex):
+	default_field_name = 'src'
+	default_interface = ICourseOutlineNode
+
+class NodeContentUnitIndex(ValueIndex):
+	default_field_name = 'ContentNTIID'
+	default_interface = ICourseOutlineNode
+
+class NodeLessonOverviewIndex(ValueIndex):
+	default_field_name = 'LessonOverviewNTIID'
+	default_interface = ICourseOutlineNode
+
+class ValidatingAvailableBeginning(object):
+
+	__slots__ = (b'AvailableBeginning',)
+
+	def __init__(self, obj, default=None):
+		if 		ICourseOutlineCalendarNode.providedBy(obj) \
+			and obj.AvailableBeginning is not None:
+			self.AvailableBeginning = time.mktime(obj.AvailableBeginning.timetuple())
+
+	def __reduce__(self):
+		raise TypeError()
+
+class NodeAvailableBeginningRawIndex(RawIntegerValueIndex):
+	pass
+
+def NodeAvailableBeginningIndex(family=None):
+	return NormalizationWrapper(field_name='AvailableBeginning',
+								interface=ValidatingAvailableBeginning,
+								index=NodeAvailableBeginningRawIndex(family=family),
+								normalizer=TimestampToNormalized64BitIntNormalizer())
+	
+class ValidatingAvailableEnding(object):
+
+	__slots__ = (b'AvailableEnding',)
+
+	def __init__(self, obj, default=None):
+		if 		ICourseOutlineCalendarNode.providedBy(obj) \
+			and obj.AvailableEnding is not None:
+			self.AvailableEnding = time.mktime(obj.AvailableEnding.timetuple())
+
+	def __reduce__(self):
+		raise TypeError()
+
+class NodeAvailableEndingRawIndex(RawIntegerValueIndex):
+	pass
+
+def NodeAvailableEndingIndex(family=None):
+	return NormalizationWrapper(field_name='AvailableEnding',
+								interface=ValidatingAvailableBeginning,
+								index=NodeAvailableEndingRawIndex(family=family),
+								normalizer=TimestampToNormalized64BitIntNormalizer())
+
+@interface.implementer(ICatalog)
+class CourseOutlineCatalog(Catalog):
+	pass
+
+def install_course_outline_catalog(site_manager_container, intids=None):
+	lsm = site_manager_container.getSiteManager()
+	intids = lsm.getUtility(IIntIds) if intids is None else intids
+	catalog = lsm.queryUtility(ICatalog, name=COURSE_OUTLINE_CATALOG_NAME)
+	if catalog is not None:
+		return catalog
+
+	catalog = CourseOutlineCatalog(family=intids.family)
+	locate(catalog, site_manager_container, COURSE_OUTLINE_CATALOG_NAME)
+	intids.register(catalog)
+	lsm.registerUtility(catalog, provided=ICatalog, name=COURSE_OUTLINE_CATALOG_NAME)
+
+	for name, clazz in ((IX_SOURCE, NodeSrcIndex),
+						(IX_CONTENT_UNIT, NodeContentUnitIndex),
+						(IX_LESSON_OVERVIEW, NodeLessonOverviewIndex),
+						(IX_AVAILABLE_ENDING, NodeAvailableEndingIndex),
+						(IX_AVAILABLE_BEGINNING, NodeAvailableBeginningIndex)):
 		index = clazz(family=intids.family)
 		intids.register(index)
 		locate(index, catalog, name)
