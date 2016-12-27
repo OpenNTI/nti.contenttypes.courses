4f5305a226d9190b3c74e8fe93939f9d4a144c30
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-27 20:42:52 +0000

    add evolution to fix course discussions lineage
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@103300 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/generations/evolve27.py
+++ src/nti/contenttypes/courses/generations/evolve27.py
@@ -9,20 +9,15 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 26
+generation = 27
 
 from zope import component
 from zope import interface
 
 from zope.component.hooks import site as current_site
 
-from zope.intid.interfaces import IIntIds
-
 from nti.contenttypes.courses.interfaces import ICourseCatalog
-from nti.contenttypes.courses.interfaces import ICourseOutline
 from nti.contenttypes.courses.interfaces import ICourseInstance
-
-from nti.contenttypes.courses.index import install_course_outline_catalog
 
 from nti.dataserver.interfaces import IDataserver
 from nti.dataserver.interfaces import IOIDResolver
@@ -42,22 +37,17 @@ class MockDataserver(object):
 			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
 		return None
 
-def _index_nodes(outline_index, intids, seen, course_catalog):
-	for entry in course_catalog.iterCatalogEntries():
+def _fix_lineage(seen, catalog):
+	for entry in catalog.iterCatalogEntries():
 		if entry.ntiid in seen:
 			continue
 		seen.add(entry.ntiid)
 		course = ICourseInstance(entry, None)
 		if course is not None:
-			def recur(node):
-				for child in node.values():
-					recur(child)
-				uid = intids.queryId(node)
-				if uid is not None and not ICourseOutline.providedBy(node):
-					outline_index.index_doc(uid, node)
-
-		if course.Outline:
-			recur(course.Outline)
+			discussions = course.Discussions
+			if discussions.__parent__ is not course or discussions.__parent__ is None:
+				logger.warn("Fixing discussions lineage for %s", entry.ntiid)
+				discussions.__parent__ = course
 
 def do_evolve(context, generation=generation):
 	conn = context.connection
@@ -70,22 +60,18 @@ def do_evolve(context, generation=generation):
 	with current_site(ds_folder):
 		assert	component.getSiteManager() == ds_folder.getSiteManager(), \
 				"Hooks not installed?"
-		lsm = ds_folder.getSiteManager()
-		intids = lsm.getUtility(IIntIds)
-		outline_index = install_course_outline_catalog(ds_folder, intids)
-
 		seen = set()
 		for site in get_all_host_sites():
 			with current_site(site):
 				course_catalog = component.queryUtility(ICourseCatalog)
 				if course_catalog is not None and not course_catalog.isEmpty():
-					_index_nodes(outline_index, intids, seen, course_catalog)
+					_fix_lineage(seen, course_catalog)
 
 	component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
 	logger.info('Evolution %s done.', generation)
 
 def evolve(context):
 	"""
-	Evolve to generation 26 by installing the outline catalog.
+	Evolve to generation 27 by fixing discussions lineage.
 	"""
 	do_evolve(context, generation)
