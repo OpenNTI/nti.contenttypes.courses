7fa65b1399762e0fe8d610c89a36809bf7903b5c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-31 22:37:59 +0000

    implement _CourseContentBundleIO
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@105565 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/_bundle.py
+++ src/nti/contenttypes/courses/_bundle.py
@@ -5,24 +5,35 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
+from feedgenerator.django.utils.functional import lazy
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 from six import string_types
 
+from zope import component
 from zope import interface
 from zope import lifecycleevent
 
 from nti.contentlibrary.bundle import PersistentContentPackageBundle
 
+from nti.contentlibrary.externalization import ContentBundleIO
+
 from nti.contentlibrary.interfaces import IContentPackage
+from nti.contentlibrary.interfaces import IContentPackageLibrary
 
 from nti.contenttypes.courses.legacy_catalog import _ntiid_from_entry
 
 from nti.contenttypes.courses.interfaces import ICourseContentPackageBundle
 
+from nti.externalization.datastructures import InterfaceObjectIO
+
+from nti.externalization.interfaces import StandardExternalFields
+
 from nti.ntiids.ntiids import find_object_with_ntiid
+
+ITEMS = StandardExternalFields.ITEMS
 
 
 @interface.implementer(ICourseContentPackageBundle)
@@ -56,3 +67,31 @@ def created_content_package_bundle(course, bucket=None):
         lifecycleevent.created(bundle)
         created_bundle = True
     return created_bundle
+
+
+@component.adapter(ICourseContentPackageBundle)
+class _CourseContentBundleIO(ContentBundleIO):
+
+    _ext_iface_upper_bound = ICourseContentPackageBundle
+
+    _excluded_in_ivars_ = ContentBundleIO._excluded_in_ivars_.union(
+        {'ntiid', 'root', 'ContentPackages'})
+
+    def resolve(self, ntiid, library):
+        paths = library.pathToNTIID(ntiid) if library is not None else None
+        return paths[0] if paths else None
+
+    def updateFromExternalObject(self, parsed, *args, **kwargs):
+        result = InterfaceObjectIO.updateFromExternalObject(self, parsed)
+        items = parsed.get('ContentPackages') or parsed.get(ITEMS)
+        library = component.queryUtility(IContentPackageLibrary)
+        if items is not None:
+            packages = []
+            for ntiid in items:
+                package = self.resolve(ntiid, library)
+                if package is None:
+                    raise KeyError("Cannot find content package", ntiid)
+                packages.append(package)
+            self._ext_self.ContentPackages = packages
+            result = True
+        return result
