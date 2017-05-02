6959c7e8c028903fe9fc7a3ae400209b82eb21fa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-02 18:34:13 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@111924 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/decorators.py
+++ src/nti/contenttypes/courses/decorators.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -40,5 +40,5 @@ class _CourseOutlineDecorator(object):
     __metaclass__ = SingletonDecorator
 
     def decorateExternalObject(self, original, external):
-        external.pop('publishBeginning', None)
         external.pop('publishEnding', None)
+        external.pop('publishBeginning', None)
