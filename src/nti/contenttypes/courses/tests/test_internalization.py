900b1072a85af5749343db8f16c444cd60cdc622
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-03 18:51:56 +0000

    add test for internalization
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@112004 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/tests/test_internalization.py
+++ src/nti/contenttypes/courses/tests/test_internalization.py
@@ -7,7 +7,11 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import none
 from hamcrest import is_not
+from hamcrest import has_length
+from hamcrest import assert_that
+from hamcrest import has_entries
 does_not = is_not
 
 import os
@@ -17,6 +21,7 @@ import simplejson
 
 from nti.contenttypes.courses.legacy_catalog import PersistentCourseCatalogLegacyEntry as CourseCatalogLegacyEntry
 
+from nti.externalization.externalization import toExternalObject
 from nti.externalization.internalization import update_from_external_object
 
 from nti.contenttypes.courses.tests import CourseLayerTest
@@ -35,3 +40,17 @@ class TestInternalization(CourseLayerTest):
 
         entry = CourseCatalogLegacyEntry()
         update_from_external_object(entry, json_data)
+        ext_obj = toExternalObject(entry)
+        assert_that(ext_obj,
+                    has_entries('Preview', False,
+                                "StartDate", "2016-08-22T05:00:00Z",
+                                "EndDate", "2016-12-19T05:00:00Z",
+                                "Schedule", has_entries("days", ["MTWRF"],
+                                                        "times", has_length(2)),
+                                "Instructors", has_length(5),
+                                "ProviderUniqueID", "BIOL 2124",
+                                "ProviderDepartmentTitle", "Department of Biology, University of Oklahoma",
+                                "description", is_not(none()),
+                                "Credit", has_length(1),
+                                "title", "Human Physiology",
+                                "Video", "kaltura://1500101/0_gpczmps5/"))
