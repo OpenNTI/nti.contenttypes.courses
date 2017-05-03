ecc0b222d9cef4987aa3268da9bc8c8551d9be09
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-03 15:56:11 +0000

    test mimetype
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@111986 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/tests/test_catalog_parser.py
+++ src/nti/contenttypes/courses/tests/test_catalog_parser.py
@@ -7,15 +7,15 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-from hamcrest import all_of
-from hamcrest import contains_string
 from hamcrest import is_
 from hamcrest import none
+from hamcrest import all_of
 from hamcrest import is_not
 from hamcrest import equal_to
 from hamcrest import assert_that
 from hamcrest import has_entries
 from hamcrest import has_properties
+from hamcrest import contains_string
 does_not = is_not
 
 from nti.testing.matchers import verifiably_provides
@@ -41,137 +41,150 @@ from nti.externalization.tests import externalizes
 
 from nti.contenttypes.courses.tests import CourseLayerTest
 
+
 class TestCatalogParser(CourseLayerTest):
 
-	def setUp(self):
-		path = os.path.join( os.path.dirname(__file__),
-							 'TestSynchronizeWithSubInstances',
-							 'Spring2014',
-							 'Gateway',
-							 'course_info.json')
-		key = FilesystemKey()
-		key.absolute_path = path
-		self.key = key
+    def setUp(self):
+        path = os.path.join(os.path.dirname(__file__),
+                            'TestSynchronizeWithSubInstances',
+                            'Spring2014',
+                            'Gateway',
+                            'course_info.json')
+        key = FilesystemKey()
+        key.absolute_path = path
+        self.key = key
 
-	def test_trivial_parse(self):
-		key = self.key
+    def test_trivial_parse(self):
+        key = self.key
 
-		entry = CourseCatalogLegacyEntry()
-		fill_entry_from_legacy_key(entry, key)
+        entry = CourseCatalogLegacyEntry()
+        fill_entry_from_legacy_key(entry, key)
 
-		assert_that( entry,
-					 has_properties( 'ProviderUniqueID', 'CLC 3403',
-									 'Title', 'Law and Justice',
-									 'DisableOverviewCalendar', True) )
-		assert_that(entry, verifiably_provides(ICourseCatalogLegacyEntry))
+        assert_that(entry,
+                    has_properties('ProviderUniqueID', 'CLC 3403',
+                                   'Title', 'Law and Justice',
+                                   'DisableOverviewCalendar', True))
+        assert_that(entry, verifiably_provides(ICourseCatalogLegacyEntry))
 
-		richDescription = entry.RichDescription
-		assert_that(richDescription, all_of(contains_string('<strong>'), contains_string('<li>')))
+        richDescription = entry.RichDescription
+        assert_that(richDescription, 
+                    all_of(contains_string('<strong>'), contains_string('<li>')))
 
-		assert_that( entry,
-					 externalizes( has_entries(
-							 'DisableOverviewCalendar', True,
-							 'RichDescription', contains_string('<strong>')) ) )
+        assert_that(entry,
+                    externalizes(has_entries(
+                                    'MimeType', 'application/vnd.nextthought.courses.persistentcoursecataloglegacyentry',
+                                    'DisableOverviewCalendar', True,
+                                    'RichDescription', contains_string('<strong>'))))
 
-		fill_entry_from_legacy_key(entry, key)
-		assert_that( entry,
-					 has_properties( 'ProviderUniqueID', 'CLC 3403',
-									 'Title', 'Law and Justice',
-									 'AdditionalProperties', {"section":"parent"}) )
+        fill_entry_from_legacy_key(entry, key)
+        assert_that(entry,
+                    has_properties('ProviderUniqueID', 'CLC 3403',
+                                   'Title', 'Law and Justice',
+                                   'AdditionalProperties', {"section": "parent"}))
 
-	def test_start_and_duration_parse(self):
-		key = self.key
+    def test_start_and_duration_parse(self):
+        key = self.key
 
-		entry = CourseCatalogLegacyEntry()
-		fill_entry_from_legacy_key(entry, key)
+        entry = CourseCatalogLegacyEntry()
+        fill_entry_from_legacy_key(entry, key)
 
-		json = key.readContentsAsJson()
-		json['startDate'] = '2015-06-29T05:00:00+00:00'
-		del json['duration']
+        json = key.readContentsAsJson()
+        json['startDate'] = '2015-06-29T05:00:00+00:00'
+        del json['duration']
 
-		fill_entry_from_legacy_json(entry, json)
+        fill_entry_from_legacy_json(entry, json)
 
-		assert_that(entry.StartDate, is_not(none()))
-		assert_that(entry.Duration, is_(none()))
-		assert_that(entry.EndDate, is_(none()))
+        assert_that(entry.StartDate, is_not(none()))
+        assert_that(entry.Duration, is_(none()))
+        assert_that(entry.EndDate, is_(none()))
 
-		json['duration'] = '1 Weeks'
-		fill_entry_from_legacy_json(entry, json)
-		assert_that(entry.Duration, equal_to(timedelta(days=7)))
-		assert_that(entry.EndDate, equal_to(IDateTime('2015-07-06T05:00:00+00:00')))
+        json['duration'] = '1 Weeks'
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry.Duration, equal_to(timedelta(days=7)))
+        assert_that(entry.EndDate, 
+                    equal_to(IDateTime('2015-07-06T05:00:00+00:00')))
 
-		# Now pop
-		del json['startDate']
-		fill_entry_from_legacy_json(entry, json)
+        # Now pop
+        del json['startDate']
+        fill_entry_from_legacy_json(entry, json)
 
-		assert_that(entry.StartDate, none())
-		assert_that(entry.Duration, is_not(none()))
-		assert_that(entry.EndDate, none())
+        assert_that(entry.StartDate, none())
+        assert_that(entry.Duration, is_not(none()))
+        assert_that(entry.EndDate, none())
 
-	def test_start_and_end_date_parse(self):
-		key = self.key
+    def test_start_and_end_date_parse(self):
+        key = self.key
 
-		entry = CourseCatalogLegacyEntry()
-		fill_entry_from_legacy_key(entry, key)
+        entry = CourseCatalogLegacyEntry()
+        fill_entry_from_legacy_key(entry, key)
 
-		json = key.readContentsAsJson()
-		json['startDate'] = '2015-06-29T05:00:00+00:00'
-		json['duration'] = '1 Weeks'
-		json['endDate'] = '2015-06-30T05:00:00+00:00'
-		fill_entry_from_legacy_json(entry, json)
+        json = key.readContentsAsJson()
+        json['startDate'] = '2015-06-29T05:00:00+00:00'
+        json['duration'] = '1 Weeks'
+        json['endDate'] = '2015-06-30T05:00:00+00:00'
+        fill_entry_from_legacy_json(entry, json)
 
-		assert_that(entry.StartDate, equal_to(IDateTime('2015-06-29T05:00:00+00:00')))
-		assert_that(entry.Duration, equal_to(timedelta(days=7)))
-		assert_that(entry.EndDate, equal_to(IDateTime('2015-06-30T05:00:00+00:00')))
+        assert_that(entry.StartDate, 
+                    equal_to(IDateTime('2015-06-29T05:00:00+00:00')))
+        assert_that(entry.Duration, 
+                    equal_to(timedelta(days=7)))
+        assert_that(entry.EndDate, 
+                    equal_to(IDateTime('2015-06-30T05:00:00+00:00')))
 
-	def test_toggle_non_public(self):
-		key = self.key
+    def test_toggle_non_public(self):
+        key = self.key
 
-		entry = CourseCatalogLegacyEntry()
-		fill_entry_from_legacy_key(entry, key)
+        entry = CourseCatalogLegacyEntry()
+        fill_entry_from_legacy_key(entry, key)
 
-		json = key.readContentsAsJson()
+        json = key.readContentsAsJson()
 
-		json['is_non_public'] = True
-		fill_entry_from_legacy_json(entry, json)
-		assert_that( entry, verifiably_provides(INonPublicCourseInstance))
+        json['is_non_public'] = True
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry, verifiably_provides(INonPublicCourseInstance))
 
-		json['is_non_public'] = False
-		fill_entry_from_legacy_json(entry, json)
-		assert_that( entry, does_not(verifiably_provides(INonPublicCourseInstance)))
+        json['is_non_public'] = False
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry, 
+                    does_not(verifiably_provides(INonPublicCourseInstance)))
 
-		# Back to true
-		json['is_non_public'] = True
-		fill_entry_from_legacy_json(entry, json)
-		assert_that( entry, verifiably_provides(INonPublicCourseInstance))
+        # Back to true
+        json['is_non_public'] = True
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry, verifiably_provides(INonPublicCourseInstance))
 
-		# Now simply missing
-		del json['is_non_public']
-		fill_entry_from_legacy_json(entry, json)
-		assert_that( entry, does_not(verifiably_provides(INonPublicCourseInstance)))
+        # Now simply missing
+        del json['is_non_public']
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry,
+                    does_not(verifiably_provides(INonPublicCourseInstance)))
 
-	def test_toggle_anonymously_accessible(self):
-		key = self.key
+    def test_toggle_anonymously_accessible(self):
+        key = self.key
 
-		entry = CourseCatalogLegacyEntry()
-		fill_entry_from_legacy_key(entry, key)
+        entry = CourseCatalogLegacyEntry()
+        fill_entry_from_legacy_key(entry, key)
 
-		json = key.readContentsAsJson()
+        json = key.readContentsAsJson()
 
-		json['is_anonymously_but_not_publicly_accessible'] = True
-		fill_entry_from_legacy_json(entry, json)
-		assert_that( entry, verifiably_provides(IAnonymouslyAccessibleCourseInstance))
+        json['is_anonymously_but_not_publicly_accessible'] = True
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry,
+                    verifiably_provides(IAnonymouslyAccessibleCourseInstance))
 
-		json['is_anonymously_but_not_publicly_accessible'] = False
-		fill_entry_from_legacy_json(entry, json)
-		assert_that( entry, does_not(verifiably_provides(IAnonymouslyAccessibleCourseInstance)))
+        json['is_anonymously_but_not_publicly_accessible'] = False
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry, 
+                    does_not(verifiably_provides(IAnonymouslyAccessibleCourseInstance)))
 
-		# Back to true
-		json['is_anonymously_but_not_publicly_accessible'] = True
-		fill_entry_from_legacy_json(entry, json)
-		assert_that( entry, verifiably_provides(IAnonymouslyAccessibleCourseInstance))
+        # Back to true
+        json['is_anonymously_but_not_publicly_accessible'] = True
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry, 
+                    verifiably_provides(IAnonymouslyAccessibleCourseInstance))
 
-		# Now simply missing
-		del json['is_anonymously_but_not_publicly_accessible']
-		fill_entry_from_legacy_json(entry, json)
-		assert_that( entry, does_not(verifiably_provides(IAnonymouslyAccessibleCourseInstance)))
+        # Now simply missing
+        del json['is_anonymously_but_not_publicly_accessible']
+        fill_entry_from_legacy_json(entry, json)
+        assert_that(entry, 
+                    does_not(verifiably_provides(IAnonymouslyAccessibleCourseInstance)))
