694b0d723c8205515a5dfa9505401d8625135888
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-31 20:39:20 +0000

    use new predicate location
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@113916 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/predicates.py
+++ src/nti/contenttypes/courses/predicates.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -14,11 +14,11 @@ from zope import component
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import ISystemUserPrincipal
 
+from nti.dataserver.metadata.predicates import BasePrincipalObjects
+
 from nti.contenttypes.courses.interfaces import ICourseCatalog
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import IPrincipalEnrollments
-
-from nti.metadata.predicates import BasePrincipalObjects
 
 from nti.site.hostpolicy import run_job_in_all_host_sites
 
