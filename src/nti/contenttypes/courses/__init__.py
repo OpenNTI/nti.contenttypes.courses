444779a126641d17a70affc326a71812e532d1f4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-05 23:01:32 +0000

    remove unicode literals
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@114312 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/__init__.py
+++ src/nti/contenttypes/courses/__init__.py
@@ -6,7 +6,7 @@ General objects and support for courses.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -14,14 +14,15 @@ logger = __import__('logging').getLogger(__name__)
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory(__name__)
 
-from nti.contenttypes.courses.utils import get_courses_catalog
+from nti.contenttypes.courses.index import get_courses_catalog
+from nti.contenttypes.courses.index import get_enrollment_catalog
+
 from nti.contenttypes.courses.utils import get_course_vendor_info
-from nti.contenttypes.courses.utils import get_enrollment_catalog
 
 from nti.contenttypes.courses.vendorinfo import VENDOR_INFO_KEY
 
 #: SubInstances constants
-SUB_INSTANCES = 'SubInstances'
+SUB_INSTANCES = u'SubInstances'
 
 #: Course role info file name
 ROLE_INFO_NAME = 'role_info.json'
