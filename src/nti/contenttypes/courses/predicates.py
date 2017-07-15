f88d174ae0766aea7aa9d776bf6385f61c8c457c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-15 22:55:39 +0000

    add predicate
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contenttypes.courses/trunk@117538 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contenttypes/courses/predicates.py
+++ src/nti/contenttypes/courses/predicates.py
@@ -11,13 +11,18 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 
+from nti.coremetadata.interfaces import SYSTEM_USER_ID
+from nti.coremetadata.interfaces import SYSTEM_USER_NAME
+
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import ISystemUserPrincipal
 
+from nti.dataserver.metadata.predicates import BoardObjectsMixin
 from nti.dataserver.metadata.predicates import BasePrincipalObjects
 
 from nti.contenttypes.courses.interfaces import ICourseCatalog
 from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseEnrollments
 from nti.contenttypes.courses.interfaces import IPrincipalEnrollments
 
 
@@ -43,24 +48,51 @@ def outline_nodes_collector(course):
 
 
 @component.adapter(ISystemUserPrincipal)
-class _CoursePrincipalObjects(BasePrincipalObjects):
+class _CoursePrincipalObjects(BasePrincipalObjects, BoardObjectsMixin):
+
+    system = (None, SYSTEM_USER_ID, SYSTEM_USER_NAME)
+
+    def board_objects(self, board):
+        generator = super(_CoursePrincipalObjects, self).board_objects(board)
+        for item in generator:
+            if self.creator(item) in self.system:
+                yield item
 
     def iter_objects(self):
         result = []
         for course in course_collector():
             result.append(course)
-            for node in outline_nodes_collector(course):
-                result.append(node)
+            result.append(course.SharingScopes)
+            result.extend(course.SharingScopes.values())
+            result.extend(outline_nodes_collector(course))
+            result.extend(self.board_objects(course.Discussions))
         return result
 
 
 @component.adapter(IUser)
-class _EnrollmentPrincipalObjects(BasePrincipalObjects):
+class _EnrollmentPrincipalObjects(BasePrincipalObjects, BoardObjectsMixin):
 
     def iter_objects(self):
         result = []
         for enrollments in component.subscribers((self.user,),
                                                  IPrincipalEnrollments):
-            for enrollment in enrollments.iter_enrollments():
-                result.append(enrollment)
+            result.extend(enrollments.iter_enrollments())
+        return result
+
+
+@component.adapter(IUser)
+class _UserBoardPrincipalObjects(BasePrincipalObjects, BoardObjectsMixin):
+
+    def board_objects(self, board):
+        username = self.user.username.lower()
+        for item in super(_UserBoardPrincipalObjects, self).board_objects(board):
+            if self.creator(item) == username:
+                yield item
+
+    def iter_objects(self):
+        result = []
+        for course in course_collector():
+            enrollments = ICourseEnrollments(course)
+            if enrollments.is_principal_enrolled(self.user):
+                result.extend(self.board_objects(course.Discussions))
         return result
