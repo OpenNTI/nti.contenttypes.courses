5067dc31e228a6dd5af1200b0ea81cd9a7b02045
Carlos Sanchez <papachoco@gmail.com>
2017-10-15 13:57:54 -0500

    use Singleton class

--- src/nti/contenttypes/courses/decorators.py
+++ src/nti/contenttypes/courses/decorators.py
@@ -8,8 +8,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import six
-
 from zope import component
 from zope import interface
 
@@ -23,20 +21,16 @@ from nti.contenttypes.courses.legacy_catalog import ICourseCatalogInstructorLega
 from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.interfaces import IExternalObjectDecorator
 
-from nti.externalization.singleton import SingletonMetaclass
+from nti.externalization.singleton import Singleton
 
 MIMETYPE = StandardExternalFields.MIMETYPE
 
 logger = __import__('logging').getLogger(__name__)
 
 
-@six.add_metaclass(SingletonMetaclass)
 @component.adapter(ICourseOutlineNode)
 @interface.implementer(IExternalObjectDecorator)
-class _CourseOutlineNodeDecorator(object):
-
-    def __init__(self, *args):
-        pass
+class _CourseOutlineNodeDecorator(Singleton):
 
     def decorateExternalObject(self, original, external):
         if not original.LessonOverviewNTIID:
@@ -45,49 +39,33 @@ class _CourseOutlineNodeDecorator(object):
             external['ntiid'] = original.ntiid
 
 
-@six.add_metaclass(SingletonMetaclass)
 @component.adapter(ICourseOutline)
 @interface.implementer(IExternalObjectDecorator)
-class _CourseOutlineDecorator(object):
-
-    def __init__(self, *args):
-        pass
+class _CourseOutlineDecorator(Singleton):
 
     def decorateExternalObject(self, unused_original, external):
         external.pop('publishEnding', None)
         external.pop('publishBeginning', None)
 
 
-@six.add_metaclass(SingletonMetaclass)
 @interface.implementer(IExternalObjectDecorator)
 @component.adapter(ICourseCatalogInstructorLegacyInfo)
-class _InstructorLegacyInfoDecorator(object):
-
-    def __init__(self, *args):
-        pass
+class _InstructorLegacyInfoDecorator(Singleton):
 
     def decorateExternalObject(self, unused_original, external):
         external.pop('userid', None)
 
 
-@six.add_metaclass(SingletonMetaclass)
 @component.adapter(ICourseInstanceSharingScope)
 @interface.implementer(IExternalObjectDecorator)
-class _CourseInstanceSharingScopeDecorator(object):
-
-    def __init__(self, *args):
-        pass
+class _CourseInstanceSharingScopeDecorator(Singleton):
 
     def decorateExternalObject(self, unused_original, external):
         external[MIMETYPE] = 'application/vnd.nextthought.community'
 
 
-@six.add_metaclass(SingletonMetaclass)
 @interface.implementer(IExternalObjectDecorator)
-class _CourseNonPublicStatusDecorator(object):
-
-    def __init__(self, *args):
-        pass
+class _CourseNonPublicStatusDecorator(Singleton):
 
     def decorateExternalObject(self, original, external):
         if 'is_non_public' not in external:
