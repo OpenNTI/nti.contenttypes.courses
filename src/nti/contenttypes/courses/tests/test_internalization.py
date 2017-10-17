a590d26a432c86034bd290ccf0a94abae92b9345
Carlos Sanchez <papachoco@gmail.com>
2017-10-17 16:19:57 -0500

    pylint

--- src/nti/contenttypes/courses/tests/test_internalization.py
+++ src/nti/contenttypes/courses/tests/test_internalization.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
@@ -24,10 +25,11 @@ from nti.contenttypes.courses.internalization import legacy_to_schema_transform
 
 from nti.contenttypes.courses.legacy_catalog import PersistentCourseCatalogLegacyEntry
 
-from nti.externalization.externalization import toExternalObject
-from nti.externalization.internalization import update_from_external_object
-
 from nti.contenttypes.courses.tests import CourseLayerTest
+
+from nti.externalization.externalization import toExternalObject
+
+from nti.externalization.internalization import update_from_external_object
 
 
 class TestInternalization(CourseLayerTest):
--- src/nti/contenttypes/courses/tests/test_outlines.py
+++ src/nti/contenttypes/courses/tests/test_outlines.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
@@ -49,102 +50,109 @@ from nti.recorder.interfaces import ITransactionRecordHistory
 
 from nti.contenttypes.courses.tests import CourseLayerTest
 
+
 class TestCourseOutline(CourseLayerTest):
 
-	def test_outline_implements(self):
-		assert_that(outlines.CourseOutlineNode(), verifiably_provides(interfaces.ICourseOutlineNode))
-		assert_that(outlines.CourseOutline(), verifiably_provides(interfaces.ICourseOutline))
-		assert_that(outlines.CourseOutlineNode(), verifiably_provides(IRecordable))
-		assert_that(outlines.CourseOutline(), verifiably_provides(IRecordable))
+    def test_outline_implements(self):
+        assert_that(outlines.CourseOutlineNode(),
+                    verifiably_provides(interfaces.ICourseOutlineNode))
+        assert_that(outlines.CourseOutline(),
+                    verifiably_provides(interfaces.ICourseOutline))
+        assert_that(outlines.CourseOutlineNode(),
+                    verifiably_provides(IRecordable))
+        assert_that(outlines.CourseOutline(), verifiably_provides(IRecordable))
 
-	def test_outline_tag_fields(self):
-		fields = ('title', 'description', 'AvailableEnding', 'AvailableBeginning')
-		for iface in (interfaces.ICourseOutlineNode,
-					  interfaces.ICourseOutlineContentNode,
-					  interfaces.ICourseOutlineCalendarNode):
+    def test_outline_tag_fields(self):
+        fields = ('title', 'description',
+                  'AvailableEnding', 'AvailableBeginning')
+        for iface in (interfaces.ICourseOutlineNode,
+                      interfaces.ICourseOutlineContentNode,
+                      interfaces.ICourseOutlineCalendarNode):
 
-			for name in fields:
-				if name in iface:
-					value = iface[name].queryTaggedValue(interfaces.TAG_HIDDEN_IN_UI)
-					assert_that(value, is_(False))
-					value = iface[name].queryTaggedValue(interfaces.TAG_REQUIRED_IN_UI)
-					assert_that(value, is_(False))
+            for name in fields:
+                if name in iface:
+                    value = iface[name].queryTaggedValue(
+                        interfaces.TAG_HIDDEN_IN_UI)
+                    assert_that(value, is_(False))
+                    value = iface[name].queryTaggedValue(
+                        interfaces.TAG_REQUIRED_IN_UI)
+                    assert_that(value, is_(False))
 
-			for k, v in iface.namesAndDescriptions(all=True):
-				if k not in fields and not IMethod.providedBy(v):
-					value = v.queryTaggedValue(interfaces.TAG_HIDDEN_IN_UI)
-					assert_that(value, is_(True))
+            for k, v in iface.namesAndDescriptions(all=True):
+                if k not in fields and not IMethod.providedBy(v):
+                    value = v.queryTaggedValue(interfaces.TAG_HIDDEN_IN_UI)
+                    assert_that(value, is_(True))
 
-	def test_trx_history(self):
-		node = outlines.CourseOutlineNode()
-		history = ITransactionRecordHistory(node, None)
-		assert_that(history, is_not(none()))
+    def test_trx_history(self):
+        node = outlines.CourseOutlineNode()
+        history = ITransactionRecordHistory(node, None)
+        assert_that(history, is_not(none()))
 
-	def test_outline_containment(self):
-		inst = courses.CourseInstance()
-		outline = inst.Outline
-		node = outlines.CourseOutlineNode()
-		outline.append(node)
-		node2 = outlines.CourseOutlineContentNode(ContentNTIID='tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON',
-												  AvailableBeginning=datetime.now())
-		node.append(node2)
+    def test_outline_containment(self):
+        inst = courses.CourseInstance()
+        outline = inst.Outline
+        node = outlines.CourseOutlineNode()
+        outline.append(node)
+        node2 = outlines.CourseOutlineContentNode(ContentNTIID=u'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON',
+                                                  AvailableBeginning=datetime.now())
+        node.append(node2)
 
-		assert_that(node, validly_provides(interfaces.ICourseOutlineNode))
-		assert_that(node, validly_provides(ICalendarPublishable))
-		assert_that(outline, validly_provides(interfaces.ICourseOutline))
-		assert_that(inst, validly_provides(interfaces.ICourseInstance))
+        assert_that(node, validly_provides(interfaces.ICourseOutlineNode))
+        assert_that(node, validly_provides(ICalendarPublishable))
+        assert_that(outline, validly_provides(interfaces.ICourseOutline))
+        assert_that(inst, validly_provides(interfaces.ICourseInstance))
 
-	def test_outline_externalizes(self):
-		inst = courses.CourseInstance()
-		outline = inst.Outline
-		node = outlines.CourseOutlineNode()
-		outline.append(node)
-		outline.src = 'src/is/this'
-		now = isodate.parse_datetime('2015-12-03T15:01:24Z')
-		node2 = outlines.CourseOutlineContentNode(ContentNTIID='tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON',
-												  AvailableBeginning=now)
-		outline.append(node2)
+    def test_outline_externalizes(self):
+        inst = courses.CourseInstance()
+        outline = inst.Outline
+        node = outlines.CourseOutlineNode()
+        outline.append(node)
+        outline.src = u'src/is/this'
+        now = isodate.parse_datetime('2015-12-03T15:01:24Z')
+        node2 = outlines.CourseOutlineContentNode(ContentNTIID=u'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON',
+                                                  AvailableBeginning=now)
+        outline.append(node2)
 
-		ext_obj = to_external_object(inst)
-		assert_that(ext_obj, has_entries('Class', 'CourseInstance',
-										 'Outline', has_entries('src', 'src/is/this',
-																'MimeType', 'application/vnd.nextthought.courses.courseoutline'),
-										 'MimeType', 'application/vnd.nextthought.courses.courseinstance'))
+        ext_obj = to_external_object(inst)
+        assert_that(ext_obj, has_entries('Class', 'CourseInstance',
+                                         'Outline', has_entries('src', 'src/is/this',
+                                                                'MimeType', 'application/vnd.nextthought.courses.courseoutline'),
+                                         'MimeType', 'application/vnd.nextthought.courses.courseinstance'))
 
-		ext_obj = to_external_object(node2)
-		assert_that(ext_obj, has_entries('Class', 'CourseOutlineContentNode',
-										 'MimeType', 'application/vnd.nextthought.courses.courseoutlinecontentnode',
-										 'ContentNTIID', u'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON'))
+        ext_obj = to_external_object(node2)
+        assert_that(ext_obj, has_entries('Class', 'CourseOutlineContentNode',
+                                         'MimeType', 'application/vnd.nextthought.courses.courseoutlinecontentnode',
+                                         'ContentNTIID', 'tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.lec:01_LESSON'))
 
-		ext_obj['AvailableEnding'] = now.isoformat()
-		ext_obj['AvailableBeginning'] = time.mktime(now.timetuple())  # change to float
-		factory = find_factory_for(ext_obj)
-		assert_that(factory, is_not(none()))
-		inst = factory()
-		update_from_external_object(inst, ext_obj)
-		assert_that(inst, has_property('AvailableEnding'), is_(now))
-		assert_that(inst, has_property('AvailableBeginning'), is_(now))
-		
-	def test_outline_io_exporter(self):
-		path = os.path.join(os.path.dirname(__file__),
-							'TestSynchronizeWithSubInstances',
-							'Spring2014',
-							'Gateway',
-							'course_outline.xml')
-		key = FilesystemKey()
-		key.absolute_path = path
+        ext_obj['AvailableEnding'] = now.isoformat()
+        ext_obj['AvailableBeginning'] = time.mktime(now.timetuple())  # change to float
+        factory = find_factory_for(ext_obj)
+        assert_that(factory, is_not(none()))
+        inst = factory()
+        update_from_external_object(inst, ext_obj)
+        assert_that(inst, has_property('AvailableEnding'), is_(now))
+        assert_that(inst, has_property('AvailableBeginning'), is_(now))
 
-		outline = outlines.CourseOutline()
-		fill_outline_from_key(outline, key)
-		assert_that(outline, has_length(7))
+    def test_outline_io_exporter(self):
+        path = os.path.join(os.path.dirname(__file__),
+                            'TestSynchronizeWithSubInstances',
+                            'Spring2014',
+                            'Gateway',
+                            'course_outline.xml')
+        key = FilesystemKey()
+        key.absolute_path = path
 
-		ext_obj = to_external_object(outline, name="exporter")
-		assert_that(ext_obj, has_entry('Items', has_length(7)))
+        outline = outlines.CourseOutline()
+        fill_outline_from_key(outline, key)
+        assert_that(outline, has_length(7))
 
-		new_outline = outlines.CourseOutline()
-		update_from_external_object(new_outline, ext_obj)
-		assert_that(new_outline, has_length(7))
-		
-		for node in new_outline.values():
-			assert_that(node, has_property('ntiid', is_not(none())))
-			assert_that(is_valid_ntiid_string(node.ntiid), is_(True))
+        ext_obj = to_external_object(outline, name="exporter")
+        assert_that(ext_obj, has_entry('Items', has_length(7)))
+
+        new_outline = outlines.CourseOutline()
+        update_from_external_object(new_outline, ext_obj)
+        assert_that(new_outline, has_length(7))
+
+        for node in new_outline.values():
+            assert_that(node, has_property('ntiid', is_not(none())))
+            assert_that(is_valid_ntiid_string(node.ntiid), is_(True))
