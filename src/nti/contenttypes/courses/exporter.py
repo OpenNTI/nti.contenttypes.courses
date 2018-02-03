38993b84c5bd6f815cd7708255d964e6d2166b12
Josh Zuech <josh.zuech@nextthought.com>
2018-02-03 10:14:56 -0600

    Export a hash to ensure an export zip is only imported once per environment

--- src/nti/contenttypes/courses/exporter.py
+++ src/nti/contenttypes/courses/exporter.py
@@ -32,6 +32,8 @@ from zope.dublincore.interfaces import IWriteZopeDublinCore
 
 from zope.interface.interfaces import IMethod
 
+from zope.intid.interfaces import IIntIds
+
 from zope.proxy import isProxy
 from zope.proxy import ProxyBase
 
@@ -60,6 +62,7 @@ from nti.contenttypes.courses import ROLE_INFO_NAME
 from nti.contenttypes.courses import VENDOR_INFO_NAME
 from nti.contenttypes.courses import CATALOG_INFO_NAME
 from nti.contenttypes.courses import COURSE_OUTLINE_NAME
+from nti.contenttypes.courses import COURSE_EXPORT_HASH_FILE
 from nti.contenttypes.courses import ASSIGNMENT_POLICIES_NAME
 
 from nti.contenttypes.courses.common import get_course_packages
@@ -574,11 +577,21 @@ class CourseInfoExporter(BaseSectionExporter):
 @interface.implementer(ICourseExporter)
 class CourseExporter(object):
 
+    def _get_export_hash(self, course, salt):
+        # This should ensure we only ever import this course/salt combo once
+        # per environment.
+        intids = component.queryUtility(IIntIds)
+        course_intid = intids.getId(course)
+        return '%s_%s' % (course_intid, salt)
+
     def export(self, context, filer, backup=True, salt=None):
         now = time.time()
         salt = salt or str(time.time())
         course = ICourseInstance(context)
         entry = ICourseCatalogEntry(course)
+        export_hash = self._get_export_hash(course, salt)
+        filer.save(COURSE_EXPORT_HASH_FILE, export_hash,
+                   contentType="text/plain", overwrite=True)
         for name, exporter in sorted(component.getUtilitiesFor(ICourseSectionExporter)):
             current = time.time()
             logger.info("Processing %s", name)
