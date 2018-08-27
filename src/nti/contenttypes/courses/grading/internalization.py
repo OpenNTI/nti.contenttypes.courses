3aa80bc1493a714c9e1dbeada0304ca3daaabd66
Josh Zuech <josh.zuech@nextthought.com>
2018-08-27 18:05:14 -0500

    Add equal group grader internalizer until we bump nti.ext

--- src/nti/contenttypes/courses/grading/internalization.py
+++ src/nti/contenttypes/courses/grading/internalization.py
@@ -12,10 +12,14 @@ from zope import component
 from zope import interface
 
 from nti.contenttypes.courses.grading.interfaces import ICategoryGradeScheme
+from nti.contenttypes.courses.grading.interfaces import IEqualGroupGrader
 
 from nti.externalization.datastructures import InterfaceObjectIO
 
 from nti.externalization.interfaces import IInternalObjectUpdater
+
+from nti.externalization.internalization import find_factory_for
+from nti.externalization.internalization import update_from_external_object
 
 logger = __import__('logging').getLogger(__name__)
 
@@ -32,4 +36,27 @@ class _CategoryGradeSchemeUpdater(InterfaceObjectIO):
         if weight is not None and weight > 1:
             weight = round(weight / 100.0, 3)
             parsed['weight'] = weight
+        return InterfaceObjectIO.updateFromExternalObject(self, parsed, *args, **kwargs)
+
+
+# FIXME: We can remove this once we bump nti.externalization
+@component.adapter(IEqualGroupGrader)
+@interface.implementer(IInternalObjectUpdater)
+class _EqualGroupGraderUpdater(InterfaceObjectIO):
+
+    _ext_iface_upper_bound = IEqualGroupGrader
+
+    def parseGroups(self, parsed):
+        groups = parsed.get('Groups', {})
+        for name, value in list(groups.items()):
+            weight = value.get('Weight')
+            if weight is not None and weight > 1:
+                weight = round(weight / 100.0, 3)
+                value['weight'] = weight
+            groups[name] = scheme = find_factory_for(value)()
+            update_from_external_object(scheme, value)
+        return groups
+
+    def updateFromExternalObject(self, parsed, *args, **kwargs):
+        self.parseGroups(parsed)
         return InterfaceObjectIO.updateFromExternalObject(self, parsed, *args, **kwargs)
