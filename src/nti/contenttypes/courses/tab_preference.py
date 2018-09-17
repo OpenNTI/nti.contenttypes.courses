de9b650ab430f76ed8885366a4786a13d82a166d
Alex Yang <601474742@qq.com>
2018-09-17 14:51:05 -0500

    NTI-6014, define and store ICourseTabPreferences on ICourseInstance (#136)
    * NTI-6014, define and store ICourseTabPreferences on the ICourseInstance object
    
    * don't update order if names changed
    
    * check order type in one line
    
    * let ICourseTabPreferences extend IContained

---
+++ src/nti/contenttypes/courses/tab_preference.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from persistent.list import PersistentList
+from persistent.mapping import PersistentMapping
+
+from zope.annotation.factory import factory as an_factory
+
+from zope import component
+from zope import interface
+
+from zope.container.contained import Contained
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseTabPreferences
+
+from nti.dublincore.time_mixins import PersistentCreatedAndModifiedTimeObject
+
+
+@component.adapter(ICourseInstance)
+@interface.implementer(ICourseTabPreferences)
+class CourseTabPreferences(PersistentCreatedAndModifiedTimeObject, Contained):
+
+    mimeType = mime_type = "application/vnd.nextthought.courses.coursetabpreferences"
+
+    creator = None
+
+    def __init__(self):
+        self._names = PersistentMapping()
+        self._order = PersistentList()
+        super(CourseTabPreferences, self).__init__()
+
+    def update_names(self, names):
+        self._names = PersistentMapping()
+        self._names.update(names)
+
+    def update_order(self, order):
+        if not isinstance(order, (tuple, list)):
+            raise TypeError('order must be a tuple or a list.')
+
+        self._order = PersistentList()
+        self._order.extend(order)
+
+
+COURSE_TAB_PREFERENCES_KEY = u"CourseTabPreferences"
+_CourseTabPreferencesFactory = an_factory(CourseTabPreferences, key=COURSE_TAB_PREFERENCES_KEY)
---
+++ src/nti/contenttypes/courses/tests/test_tab_preference.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import contains
+from hamcrest import not_none
+from hamcrest import has_length
+from hamcrest import has_entries
+from hamcrest import assert_that
+from hamcrest import has_items
+from hamcrest import instance_of
+from hamcrest import raises
+from hamcrest import calling
+
+from persistent.list import PersistentList
+from persistent.mapping import PersistentMapping
+
+from nti.externalization.externalization import to_external_object
+
+from nti.externalization.internalization import find_factory_for
+from nti.externalization.internalization import update_from_external_object
+
+from nti.contenttypes.courses.courses import CourseInstance
+
+from nti.contenttypes.courses.interfaces import ICourseTabPreferences
+
+from nti.contenttypes.courses.tab_preference import CourseTabPreferences
+
+from nti.contenttypes.courses.tests import CourseLayerTest
+
+class TestCourseTabPreferences(CourseLayerTest):
+
+	def test_internalize(self):
+		external = {
+			"MimeType": "application/vnd.nextthought.courses.coursetabpreferences",
+			"names": {"1": "a", "3": "b"},
+			"order": ["1", "2"]
+		}
+		factory = find_factory_for(external)
+		assert_that(factory, not_none())
+		new_io = factory()
+		modified = new_io.lastModified
+
+		update_from_external_object(new_io, external)
+		assert_that(new_io._names, instance_of(PersistentMapping))
+		assert_that(new_io._order, instance_of(PersistentList))
+		assert_that(new_io._names, has_entries({'1': 'a', '3': 'b'}))
+		assert_that(new_io._order, has_items('1', '2'))
+		assert_that(new_io.lastModified > modified, is_(True))
+
+		update_from_external_object(new_io, {"names": None, "order": None})
+		assert_that(new_io._names, instance_of(PersistentMapping))
+		assert_that(new_io._order, instance_of(PersistentList))
+		assert_that(new_io._names, has_length(0))
+		assert_that(new_io._order, has_length(0))
+
+		update_from_external_object(new_io, {"names": {"5": "ok"}})
+		assert_that(new_io._names, instance_of(PersistentMapping))
+		assert_that(new_io._order, instance_of(PersistentList))
+		assert_that(new_io._names, has_length(1))
+		assert_that(new_io._order, has_length(0))
+
+		update_from_external_object(new_io, {"order": ['3', '4']})
+		assert_that(new_io._names, instance_of(PersistentMapping))
+		assert_that(new_io._order, instance_of(PersistentList))
+		assert_that(new_io._names, has_length(1))
+		assert_that(new_io._order, has_length(2))
+
+	def test_externalize(self):
+		obj = CourseTabPreferences()
+		assert_that(obj._names, instance_of(PersistentMapping))
+		assert_that(obj._order, instance_of(PersistentList))
+		
+		external = to_external_object(obj)
+		assert_that(external, has_entries({'MimeType': 'application/vnd.nextthought.courses.coursetabpreferences',
+										   'CreatedTime': not_none(),
+										   'Last Modified': not_none(),
+										   'names': has_length(0),
+										   'order': has_length(0)}))
+
+		obj.update_names({'1': 'a', '2': 'b'})
+		obj.update_order(['2', '3'])
+		assert_that(obj._names, instance_of(PersistentMapping))
+		assert_that(obj._order, instance_of(PersistentList))
+		
+		external = to_external_object(obj)
+		assert_that(external, has_entries({'MimeType': 'application/vnd.nextthought.courses.coursetabpreferences',
+										   'names': has_entries({'1': 'a', '2': 'b'}),
+										   'order': contains('2', '3')}))
+
+	def test_update_order(self):
+		obj = CourseTabPreferences()
+		obj.update_order(['2', '3'])
+		assert_that(obj._order, instance_of(PersistentList))
+		assert_that(obj._order, contains('2', '3'))
+
+		obj.update_order(('2', '1'))
+		assert_that(obj._order, instance_of(PersistentList))
+		assert_that(obj._order, contains('2', '1'))
+
+		assert_that(calling(obj.update_order).with_args(False), raises(TypeError, pattern="order must be a tuple or a list."))
+		assert_that(calling(obj.update_order).with_args(None), raises(TypeError, pattern="order must be a tuple or a list."))
+		assert_that(calling(obj.update_order).with_args("abc"), raises(TypeError, pattern="order must be a tuple or a list."))
+
+	def test_annotation(self):
+		course = CourseInstance()
+		obj = ICourseTabPreferences(course)
+		assert_that(obj, instance_of(CourseTabPreferences))
+		assert_that(obj.__name__, is_("CourseTabPreferences"))
+		assert_that(obj.__parent__, is_(course))
